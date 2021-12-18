f4cb6ce0a2f54cd4b94e54c099191a6ebdc29212
Gab <gabby.mcm@gmail.com>
2021-12-18 16:23:14 -0500

    Functionality complete for adding new items to library

--- script.js
+++ script.js
@@ -1,9 +1,75 @@
+const submit = document.getElementById('submit');
+
 let myLibrary = [];
 
-function Book(){
-    // the constructor
+
+
+// set up the book object
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+};
+
+let book = [];
+// create new books
+function getInputFromForm() {
+    var title = document.getElementById('title');
+    var author = document.getElementById('author');
+    var pages = document.getElementById('pages');
+    var read = document.getElementById('read');
+    submit.addEventListener('click', () => {
+        book = new Book(title.value, author.value, pages.value, read.value);
+        addBooksToLibrary(book);
+    });
+};
+
+getInputFromForm();
+
+
+
+
+function addBooksToLibrary(newBook) {
+    myLibrary.push(newBook);
+
+    var display = document.getElementById('enteredBooks');
+    var bookBox = document.createElement('div');
+    bookBox.classList.add('bookBox');
+    display.appendChild(bookBox);
+    
+    var bookTitle = document.createElement('h1');
+    bookTitle.classList.add('bookTitle');
+    bookTitle.textContent = newBook.title;
+    var bookAuthor = document.createElement('p');
+    bookAuthor.classList.add('bookAuthor');
+    bookAuthor.textContent = newBook.author;
+    var bookPages = document.createElement('p');
+    bookPages.classList.add('bookPages');
+    bookPages.textContent = newBook.pages + " pages";
+    var bookRead = document.createElement('p');
+    bookRead.classList.add('bookRead');
+    if (newBook.read.checked){
+        bookRead.textContent = "Read";
+    } else {
+        bookRead.textContent = "Not read";
+    };
+
+    /*var bookContent = document.createElement('p');
+    bookContent.textContent = newBook.title + " " + newBook.author + " " + newBook.pages + " pages " + newBook.read;
+
+    bookBox.appendChild(bookContent); */
+
+    bookBox.appendChild(bookTitle);
+    bookBox.appendChild(bookAuthor);
+    bookBox.appendChild(bookPages);
+    bookBox.appendChild(bookRead);
+
 }
 
-function addBookToLibrary(){
-    //do stuff here
-}
+// add new books to the library
+
+
+
+
+
